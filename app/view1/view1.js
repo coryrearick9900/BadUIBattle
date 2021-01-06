5dc5d8e3f9b185b1d32f55e549e856d8c73d075a
Cory Rearick <coryrearick9900@gmail.com>
2021-01-06 16:00:34 -0500

    Input is cleared and buttons are randomized on page load

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,5 +10,48 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [function() {
-
+  randomize();
+  clearText();
 }]);
+
+function randomize() {
+  const values = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  let button1 = document.getElementById("one");
+  let button2 = document.getElementById("two");
+  let button3 = document.getElementById("three");
+  let button4 = document.getElementById("four");
+  let button5 = document.getElementById("five");
+  let button6 = document.getElementById("six");
+
+  let arrayOfItems = [button1, button2, button3, button4, button5, button6];
+
+  // console.log("here");
+
+  for (let i = 0; i < arrayOfItems.length; i++) {
+    let randomNumber = Math.random()*62;
+    arrayOfItems[i].innerHTML = values.charAt(randomNumber);
+  }
+
+
+}
+
+
+// This funciton adds the value of the button to the text field
+function enterValue(buttonID) {
+  const letter = document.getElementById(buttonID).innerHTML;
+  console.log("Entering " + letter);
+  document.getElementById("Name_I").value += letter;
+
+
+}
+
+
+// Function to clear the text field value to blank
+function clearText() {
+  console.log("clearing?");
+
+  document.getElementById("Name_I").value = "";
+
+
+}
