fc4b08f2fcf87a86d1c7a4c706c6e13709151dc4
Cory Rearick <coryrearick9900@gmail.com>
2021-01-06 21:56:13 -0500

    Added layout of the Double Random page Created funcionality of the buttons to add values to the math box

--- app/double-random/double-random.js
+++ app/double-random/double-random.js
@@ -12,3 +12,22 @@ angular.module('myApp.double-random', ['ngRoute'])
     .controller('DoubleRandomController', [function() {
 
     }]);
+
+
+function addNumber(button) {
+    let input;
+    if (button === "plus") {
+        input = '+';
+    }else if (button === "minus") {
+        input = '-';
+    }else if (button === "mult") {
+        input = '*';
+    }else if (button === "div") {
+        input = '/';
+    }else {
+        input = Math.floor(Math.random()*10);
+    }
+
+    document.getElementById("math").value += input;
+
+}
